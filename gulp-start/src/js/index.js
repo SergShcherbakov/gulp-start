ec900d6ec3182200f5f2a9486cecf8fc0bfd1929
SergShcherbakov <119343134+SergShcherbakov@users.noreply.github.com>
2024-03-03 23:12:21 +0300

    first commit

---
+++ gulp-start/src/js/index.js
@@ -0,0 +1,9 @@
+
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+new AirDatepicker('#date');
+
